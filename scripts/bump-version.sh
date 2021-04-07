da077a39f6b16b2841ade6cc2eb8263fb245af2e
AidenLx <aiden.lx@outlook.com>
2021-04-07 08:28:02 +0800

    ci(release-it): update release-it script and config

---
+++ scripts/bump-version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# read minAppVersion from manifest.json
+min=$( sed -e 's/^"//' -e 's/"$//' <<< $(jq .minAppVersion manifest.json) )
+
+# write version and minAppVersion to versions.json
+jq .\"${version}\"=\"${min}\" versions.json | sponge versions.json
+
+# write minAppVersion to README.md
+sed -i '' 's/available for Obsidian v.*$/available for Obsidian v${min}+./' README.md
+
+git add versions.json README.md
