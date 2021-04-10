b8a9bec018ba8df4c037131f32a26c31263173e5
AidenLx <aiden.lx@outlook.com>
2021-04-10 10:47:16 +0800

    ci: fix bump-version.sh

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# read minAppVersion from manifest.json
+# read info from manifest.json
 min=$( sed -e 's/^"//' -e 's/"$//' <<< $(jq .minAppVersion manifest.json) )
+version=$(jq .version manifest.json)
 
 # write version and minAppVersion to versions.json
-jq .\"${version}\"=\"${min}\" versions.json | sponge versions.json
+jq .${version}=\"${min}\" versions.json | sponge versions.json
 
 # write minAppVersion to README.md
-sed -i '' 's/available for Obsidian v.*$/available for Obsidian v${min}+./' README.md
+sed -i '' "s/available for Obsidian v.*$/available for Obsidian v${min}+./" README.md
 
 git add versions.json README.md
