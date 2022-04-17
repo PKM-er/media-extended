5c92ce2e83781f915b8dd43ff69b46313dd17868
aidenlx <aiden.lx@outlook.com>
2022-04-18 01:36:23 +0800

    chore: todo for disable view input

--- packages/obsidian/src/player/ipc/hack/main-ps/disable-view-input.ts
+++ packages/obsidian/src/player/ipc/hack/main-ps/disable-view-input.ts
@@ -1,4 +1,5 @@
 import { BrowserWindow, ipcMain, webContents } from "electron";
+import { debounce } from "lodash-es";
 
 import { DisableInput } from "../const";
 
@@ -8,9 +9,12 @@ export const RegisterDisableViewInput = () => {
     if (!win) return false;
     const view = webContents.fromId(viewId);
     if (!view) return false;
+
+    // TODO: handle auto repeat and debounced keyup
     view.on("before-input-event", (evt, input) => {
       evt.preventDefault();
       win.webContents.focus();
+      console.log(input.isAutoRepeat);
       const inputevt = {
         keyCode: input.code,
         modifiers: input.modifiers as Electron.InputEvent["modifiers"],
