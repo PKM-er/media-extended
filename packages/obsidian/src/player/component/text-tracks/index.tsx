3cf14a60eb125c6a3eea92654ff76c40d024980e
aidenlx <aiden.lx@outlook.com>
2022-04-26 01:25:52 +0800

    refactor(player/component): update text track css and dom structure

--- packages/obsidian/src/player/component/text-tracks/index.tsx
+++ packages/obsidian/src/player/component/text-tracks/index.tsx
@@ -6,8 +6,13 @@ import React from "react";
 const TextTracks = () => {
   const content = useAppSelector((state) => state.interface.activeCues);
   return (
-    <div className="mx__captions">
-      <span className="mx__caption">{content}</span>
+    <div className="mx__text-track-container">
+      {/* mostly used for positioning */}
+      <div className="mx__text-track-display">
+        <div className="mx__text-track-display-backdrop">
+          <div className="mx__cue">{content}</div>
+        </div>
+      </div>
     </div>
   );
 };
