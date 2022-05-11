7965df6403426aef24ab17dd690d1d8446674915
aidenlx <aiden.lx@outlook.com>
2022-05-12 01:34:57 +0800

    refactor(player/store): split controls slice reducers

--- packages/obsidian/src/player/slice/controls/slice.ts
+++ packages/obsidian/src/player/slice/controls/slice.ts
@@ -1,7 +1,11 @@
 export const LARGE_CURRENT_TIME = 1e101;
 
 import { Fragment, getFragFromHash, is } from "@base/hash-tool";
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {
+  createSlice,
+  PayloadAction,
+  SliceCaseReducers,
+} from "@reduxjs/toolkit";
 import { isTimestamp, parseTF } from "mx-lib";
 import { parse as parseQS } from "query-string";
 
@@ -80,88 +84,263 @@ const initialState: ControlsState = {
   },
 };
 
+const alterStateReducers: SliceCaseReducers<ControlsState> = {
+  setHash: (
+    state,
+    action: PayloadAction<{ hash: string; fromLink: boolean }>,
+  ) => {
+    const { hash, fromLink } = action.payload;
+    const query = parseQS(hash),
+      frag = getFragFromHash(hash);
+    state.fragment = frag;
+    state.loop = is(query, "loop");
+    state.autoplay = is(query, "autoplay");
+    state.muted = is(query, "muted");
+
+    if (fromLink) state.paused = false;
+    // start playing when timestamp is seeked to
+    if (frag && isTimestamp(frag)) {
+      state.currentTime = frag[0];
+    }
+  },
+  setFragment: (state, action: PayloadAction<ControlsState["fragment"]>) => {
+    const frag = action.payload;
+    state.fragment = frag;
+
+    // start playing when timestamp is seeked to
+    if (frag && isTimestamp(frag)) state.paused = false;
+  },
+  play: (state) => {
+    state.paused = false;
+  },
+  pause: (state) => {
+    state.paused = true;
+  },
+  togglePlay: (state) => {
+    state.paused = !state.paused;
+  },
+
+  setFullscreen: (state, action: PayloadAction<boolean>) => {
+    state.fullscreen = action.payload;
+  },
+
+  toggleFullscreen: (state) => {
+    state.fullscreen = !state.fullscreen;
+  },
+  setPlaybackRate: (state, action: PayloadAction<number>) => {
+    if (action.payload > 0) {
+      state.playbackRate = action.payload;
+    } else {
+      state.playbackRate = 1;
+    }
+  },
+  setMute: (state, action: PayloadAction<boolean>) => {
+    state.muted = action.payload;
+  },
+  toggleMute: (state) => {
+    state.muted = !state.muted;
+  },
+  setVolume: (state, action: PayloadAction<number>) => {
+    setVolumeTo(action.payload, state);
+  },
+  setVolumeUnmute: (state, action: PayloadAction<number>) => {
+    setVolumeTo(action.payload, state);
+    state.muted = false;
+  },
+  setVolumeByOffest: (state, action: PayloadAction<number>) => {
+    setVolumeTo(state.volume + action.payload / 100, state);
+  },
+};
+
+// lock event handlers when applying state changes
+const actionLockReducers: SliceCaseReducers<ControlsState> = {
+  lockPlayPauseEvent: (state) => {
+    state.ignoreEvent.playpause = true;
+  },
+  unlockPlayPauseEvent: (state) => {
+    state.ignoreEvent.playpause = false;
+  },
+};
+
+const evtHandlerReducer: SliceCaseReducers<ControlsState> = {
+  handleLoopChange: (state, action: PayloadAction<boolean>) => {
+    state.loop = action.payload;
+  },
+  handleAutoplayChange: (state, action: PayloadAction<boolean>) => {
+    state.autoplay = action.payload;
+  },
+  handleTimeUpdate: (state, action: PayloadAction<number>) => {
+    if (action.payload !== LARGE_CURRENT_TIME)
+      state.currentTime = action.payload;
+    if (state.duration === action.payload) {
+      state.ended = true;
+    }
+  },
+  handleFullscreenChange: (state, action: PayloadAction<boolean>) => {
+    state.fullscreen = action.payload;
+  },
+  handleVolumeChange: (
+    state,
+    action: PayloadAction<{ volume: number; muted: boolean }>,
+  ) => {
+    setVolumeTo(action.payload.volume, state);
+    state.muted = action.payload.muted;
+  },
+  handleDurationChange: (state, action: PayloadAction<number | null>) => {
+    if (checkDuration(action.payload)) {
+      state.duration = action.payload;
+    } else {
+      state.duration === null;
+    }
+  },
+
+  handleSeeking: (state) => {
+    state.seeking = true;
+  },
+  handleSeeked: (state) => {
+    state.seeking = false;
+  },
+  handlePlaying: (state) => {
+    if (state.ignoreEvent.playpause) return;
+    state.paused = false;
+    state.ended = false;
+    state.waiting = false;
+    state.hasStarted = true;
+  },
+  handlePause: (state) => {
+    if (state.ignoreEvent.playpause) return;
+    state.paused = true;
+  },
+  handleRateChange: (state, action: PayloadAction<number>) => {
+    state.playbackRate = action.payload;
+  },
+  handleProgress: (
+    state,
+    action: PayloadAction<{ buffered: number; duration: number }>,
+  ) => {
+    const { buffered, duration } = action.payload;
+    if (buffered >= 0) {
+      state.buffered = buffered;
+    } else {
+      console.error("invaild buffered value", action.payload);
+    }
+    if (checkDuration(duration)) {
+      state.duration = duration;
+    }
+  },
+  handleEnded: (state) => {
+    state.ended = true;
+  },
+  handleWaiting: (state) => {
+    state.waiting = true;
+  },
+  handleError: (
+    state,
+    action: PayloadAction<{ message: string; code?: number }>,
+  ) => {
+    state.error = `${action.payload.message} (${action.payload.code})`;
+  },
+};
+
+const handleUserSeekEnd = (state: ControlsState) => {
+  if (!state.userSeek) return;
+  if (state.userSeek.pausedBeforeSeek !== null) {
+    state.paused = state.userSeek.pausedBeforeSeek;
+  }
+  // apply currentTime immediately to avoid latency from onTimeUpdate
+  state.currentTime = state.userSeek.currentTime;
+  state.userSeek = null;
+};
+const userSeekReducers: SliceCaseReducers<ControlsState> = {
+  progressBarSeek: (state, action: PayloadAction<number>) => {
+    if (state.userSeek?.source === "manual") return;
+    const source: UserSeekSource = "progress-bar";
+    let time = action.payload;
+    time = clampTime(time, state.duration);
+
+    if (state.userSeek?.source === source) {
+      // only update seek time
+      state.userSeek.currentTime = time;
+    } else {
+      // new seek action or override existing seek action
+      state.userSeek = {
+        initialTime: time,
+        currentTime: time,
+        source,
+        pausedBeforeSeek: state.paused,
+      };
+      // state.paused = true;
+    }
+  },
+  progressBarSeekEnd: (state) => {
+    if (state.userSeek?.source === "manual") return;
+    handleUserSeekEnd(state);
+  },
+  dragSeek: (state, action: PayloadAction<number>) => {
+    const source = "drag";
+    if (!state.userSeek) {
+      // new seek action
+      let time = state.currentTime;
+      state.userSeek = {
+        initialTime: time,
+        currentTime: time,
+        source,
+        pausedBeforeSeek: state.paused,
+      };
+      // state.paused = true;
+    } else if (state.userSeek.source === "drag") {
+      const forwardSeconds = action.payload;
+      const { initialTime } = state.userSeek,
+        { duration } = state;
+      state.userSeek.currentTime = clampTime(
+        forwardSeconds + initialTime,
+        duration,
+      );
+    }
+  },
+  dragSeekEnd: (state) => {
+    if (
+      state.userSeek &&
+      ["progress-bar", "manual"].includes(state.userSeek.source)
+    )
+      return;
+    handleUserSeekEnd(state);
+  },
+  requestManualSeek: (state, action: PayloadAction<number>) => {
+    const source: UserSeekSource = "manual";
+    let time = action.payload;
+    time = clampTime(time, state.duration);
+    state.userSeek = {
+      initialTime: time,
+      currentTime: time,
+      source,
+      pausedBeforeSeek: state.paused,
+    };
+    // state.paused = true;
+  },
+  requestManualOffsetSeek: (state, action: PayloadAction<number>) => {
+    const source: UserSeekSource = "manual";
+    const offset = action.payload;
+    state.userSeek = {
+      initialTime: state.currentTime,
+      currentTime: clampTime(offset + state.currentTime, state.duration),
+      source,
+      pausedBeforeSeek: state.paused,
+    };
+    // state.paused = true;
+  },
+  manualSeekDone: (state) => {
+    // highest priority, no check for source
+    handleUserSeekEnd(state);
+  },
+};
+
 export const controlsSlice = createSlice({
   name: "controls",
   initialState,
   reducers: {
-    setHash: (
-      state,
-      action: PayloadAction<{ hash: string; fromLink: boolean }>,
-    ) => {
-      const { hash, fromLink } = action.payload;
-      const query = parseQS(hash),
-        frag = getFragFromHash(hash);
-      state.fragment = frag;
-      state.loop = is(query, "loop");
-      state.autoplay = is(query, "autoplay");
-      state.muted = is(query, "muted");
-
-      if (fromLink) state.paused = false;
-      // start playing when timestamp is seeked to
-      if (frag && isTimestamp(frag)) {
-        state.currentTime = frag[0];
-      }
-    },
-    handleLoopChange: (state, action: PayloadAction<boolean>) => {
-      state.loop = action.payload;
-    },
-    handleAutoplayChange: (state, action: PayloadAction<boolean>) => {
-      state.autoplay = action.payload;
-    },
-    setFragment: (state, action: PayloadAction<ControlsState["fragment"]>) => {
-      const frag = action.payload;
-      state.fragment = frag;
-
-      // start playing when timestamp is seeked to
-      if (frag && isTimestamp(frag)) state.paused = false;
-    },
-    reset: (state) => {
+    reset: () => {
       return initialState;
-    },
-    play: (state) => {
-      state.paused = false;
-    },
-    pause: (state) => {
-      state.paused = true;
-    },
-    togglePlay: (state) => {
-      state.paused = !state.paused;
-    },
-    lockPlayPauseEvent: (state) => {
-      state.ignoreEvent.playpause = true;
-    },
-    unlockPlayPauseEvent: (state) => {
-      state.ignoreEvent.playpause = false;
-    },
-    setFullscreen: (state, action: PayloadAction<boolean>) => {
-      state.fullscreen = action.payload;
-    },
-
-    toggleFullscreen: (state) => {
-      state.fullscreen = !state.fullscreen;
-    },
-    setPlaybackRate: (state, action: PayloadAction<number>) => {
-      if (action.payload > 0) {
-        state.playbackRate = action.payload;
-      } else {
-        state.playbackRate = 1;
-      }
-    },
-    setMute: (state, action: PayloadAction<boolean>) => {
-      state.muted = action.payload;
-    },
-    toggleMute: (state) => {
-      state.muted = !state.muted;
-    },
-    setVolume: (state, action: PayloadAction<number>) => {
-      setVolumeTo(action.payload, state);
-    },
-    setVolumeUnmute: (state, action: PayloadAction<number>) => {
-      setVolumeTo(action.payload, state);
-      state.muted = false;
-    },
-    setVolumeByOffest: (state, action: PayloadAction<number>) => {
-      setVolumeTo(state.volume + action.payload / 100, state);
     },
     updateBasicInfo: (
       state,
@@ -182,160 +361,13 @@ export const controlsSlice = createSlice({
       }
       state.seeking = seeking;
     },
-    handleTimeUpdate: (state, action: PayloadAction<number>) => {
-      if (action.payload !== LARGE_CURRENT_TIME)
-        state.currentTime = action.payload;
-      if (state.duration === action.payload) {
-        state.ended = true;
-      }
-    },
-    handleFullscreenChange: (state, action: PayloadAction<boolean>) => {
-      state.fullscreen = action.payload;
-    },
-    handleVolumeChange: (
-      state,
-      action: PayloadAction<{ volume: number; muted: boolean }>,
-    ) => {
-      setVolumeTo(action.payload.volume, state);
-      state.muted = action.payload.muted;
-    },
-    handleDurationChange: (state, action: PayloadAction<number | null>) => {
-      if (checkDuration(action.payload)) {
-        state.duration = action.payload;
-      } else {
-        state.duration === null;
-      }
-    },
     revertDuration: (state, action: PayloadAction<number>) => {
       state.duration = action.payload;
     },
-    handleSeeking: (state) => {
-      state.seeking = true;
-    },
-    handleSeeked: (state) => {
-      state.seeking = false;
-    },
-    handlePlaying: (state) => {
-      if (state.ignoreEvent.playpause) return;
-      state.paused = false;
-      state.ended = false;
-      state.waiting = false;
-      state.hasStarted = true;
-    },
-    handlePause: (state) => {
-      if (state.ignoreEvent.playpause) return;
-      state.paused = true;
-    },
-    handleRateChange: (state, action: PayloadAction<number>) => {
-      state.playbackRate = action.payload;
-    },
-    handleProgress: (
-      state,
-      action: PayloadAction<{ buffered: number; duration: number }>,
-    ) => {
-      const { buffered, duration } = action.payload;
-      if (buffered >= 0) {
-        state.buffered = buffered;
-      } else {
-        console.error("invaild buffered value", action.payload);
-      }
-      if (checkDuration(duration)) {
-        state.duration = duration;
-      }
-    },
-    handleEnded: (state) => {
-      state.ended = true;
-    },
-    handleWaiting: (state) => {
-      state.waiting = true;
-    },
-    handleError: (
-      state,
-      action: PayloadAction<{ message: string; code?: number }>,
-    ) => {
-      state.error = `${action.payload.message} (${action.payload.code})`;
-    },
-    progressBarSeek: (state, action: PayloadAction<number>) => {
-      if (state.userSeek?.source === "manual") return;
-      const source: UserSeekSource = "progress-bar";
-      let time = action.payload;
-      time = clampTime(time, state.duration);
-
-      if (state.userSeek?.source === source) {
-        // only update seek time
-        state.userSeek.currentTime = time;
-      } else {
-        // new seek action or override existing seek action
-        state.userSeek = {
-          initialTime: time,
-          currentTime: time,
-          source,
-          pausedBeforeSeek: state.paused,
-        };
-        // state.paused = true;
-      }
-    },
-    progressBarSeekEnd: (state) => {
-      if (state.userSeek?.source === "manual") return;
-      handleUserSeekEnd(state);
-    },
-    dragSeek: (state, action: PayloadAction<number>) => {
-      const source = "drag";
-      if (!state.userSeek) {
-        // new seek action
-        let time = state.currentTime;
-        state.userSeek = {
-          initialTime: time,
-          currentTime: time,
-          source,
-          pausedBeforeSeek: state.paused,
-        };
-        // state.paused = true;
-      } else if (state.userSeek.source === "drag") {
-        const forwardSeconds = action.payload;
-        const { initialTime } = state.userSeek,
-          { duration } = state;
-        state.userSeek.currentTime = clampTime(
-          forwardSeconds + initialTime,
-          duration,
-        );
-      }
-    },
-    dragSeekEnd: (state) => {
-      if (
-        state.userSeek &&
-        ["progress-bar", "manual"].includes(state.userSeek.source)
-      )
-        return;
-      handleUserSeekEnd(state);
-    },
-    requestManualSeek: (state, action: PayloadAction<number>) => {
-      const source: UserSeekSource = "manual";
-      let time = action.payload;
-      time = clampTime(time, state.duration);
-      state.userSeek = {
-        initialTime: time,
-        currentTime: time,
-        source,
-        pausedBeforeSeek: state.paused,
-      };
-      // state.paused = true;
-    },
-    requestManualOffsetSeek: (state, action: PayloadAction<number>) => {
-      const source: UserSeekSource = "manual";
-      const offset = action.payload;
-      state.userSeek = {
-        initialTime: state.currentTime,
-        currentTime: clampTime(offset + state.currentTime, state.duration),
-        source,
-        pausedBeforeSeek: state.paused,
-      };
-      // state.paused = true;
-    },
-    manualSeekDone: (state) => {
-      // highest priority, no check for source
-      handleUserSeekEnd(state);
-    },
+    ...alterStateReducers,
+    ...actionLockReducers,
+    ...evtHandlerReducer,
+    ...userSeekReducers,
   },
 });
 
@@ -346,16 +378,6 @@ const clampTime = (time: number, duration: number | null) => {
     time = 0;
   }
   return time;
-};
-
-const handleUserSeekEnd = (state: ControlsState) => {
-  if (!state.userSeek) return;
-  if (state.userSeek.pausedBeforeSeek !== null) {
-    state.paused = state.userSeek.pausedBeforeSeek;
-  }
-  // apply currentTime immediately to avoid latency from onTimeUpdate
-  state.currentTime = state.userSeek.currentTime;
-  state.userSeek = null;
 };
 
 const setVolumeTo = (newVolume: number, state: ControlsState) => {
