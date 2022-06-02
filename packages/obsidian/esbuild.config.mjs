ef4a424dcc311b859a20d390e29b7a3bafdae88f
aidenlx <aiden.lx@outlook.com>
2022-06-03 00:08:38 +0800

    build: fix stack trace not working in redux devtool

--- packages/obsidian/esbuild.config.mjs
+++ packages/obsidian/esbuild.config.mjs
@@ -49,19 +49,24 @@ const remoteRedux = {
   name: "enable-remote-redux-devtools",
   setup: (build) => {
     if (isProd) return;
-    build.onLoad(
-      { filter: /src\/player\/store\/create-store\.ts$/ },
-      async (args) => ({
-        contents: (
-          `import devToolsEnhancer from "remote-redux-devtools";` +
-          (await promises.readFile(args.path, "utf8"))
-        ).replace(
-          `enhancers: []`,
-          `enhancers: [devToolsEnhancer({ realtime: true, hostname: "localhost", port: 8000, name })]`,
-        ),
-        loader: "ts",
-      }),
-    );
+    build.onLoad({ filter: /create-store\.ts$/ }, async (args) => ({
+      contents: (
+        `import devToolsEnhancer from "remote-redux-devtools";` +
+        (await promises.readFile(args.path, "utf8"))
+      ).replace(
+        `enhancers: []`,
+        `enhancers: [
+          devToolsEnhancer({
+            realtime: true,
+            hostname: "localhost",
+            port: 8000,
+            name,
+            trace: (action)=> { console.groupCollapsed(action.type, name); console.trace(action); console.groupEnd(); return new Error().stack; },
+          })
+        ]`,
+      ),
+      loader: "ts",
+    }));
   },
 };
 
--- packages/player/src/typings/redux-remote.d.ts
+++ packages/player/src/typings/redux-remote.d.ts
@@ -1,1 +1,17 @@
-declare module "remote-redux-devtools";
+import "remote-redux-devtools";
+
+type EnhancerOptions = NonNullable<
+  Exclude<import("@reduxjs/toolkit").ConfigureStoreOptions["devTools"], boolean>
+>;
+declare module "remote-redux-devtools" {
+  interface RemoteReduxDevToolsOptions {
+    maxAge?: EnhancerOptions["maxAge"];
+    trace?: EnhancerOptions["trace"];
+    traceLimit?: EnhancerOptions["traceLimit"];
+    shouldCatchErrors?: EnhancerOptions["shouldCatchErrors"];
+    shouldHotReload?: EnhancerOptions["shouldHotReload"];
+    shouldRecordChanges?: EnhancerOptions["shouldRecordChanges"];
+    shouldStartLocked?: EnhancerOptions["shouldStartLocked"];
+    pauseActionType?: EnhancerOptions["pauseActionType"];
+  }
+}
