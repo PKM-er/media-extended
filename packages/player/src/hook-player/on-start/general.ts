103dcf8d716529f68048be665c0c21041e16345a
aidenlx <aiden.lx@outlook.com>
2022-07-12 14:48:07 +0800

    fix(mx-player): fix default props not applied to player on start

--- packages/player/src/hook-player/on-start/general.ts
+++ packages/player/src/hook-player/on-start/general.ts
@@ -1,10 +1,34 @@
 import { Media } from "mx-base";
-import { PlayerStore, selectCurrentTime } from "mx-store";
+import {
+  PlayerStore,
+  selectCurrentTime,
+  selectPaused,
+  selectSpeed,
+  selectVolumeMute,
+} from "mx-store";
 
 const onStart = (player: Media, store: PlayerStore) => {
-  const currentTime = selectCurrentTime(store.getState());
+  const initState = store.getState();
+  const currentTime = selectCurrentTime(initState);
   if (currentTime && currentTime > 0) {
     player.seekTo(currentTime);
+  }
+  const paused = selectPaused(initState),
+    [muted, volume] = selectVolumeMute(initState),
+    speed = selectSpeed(initState);
+  if (paused !== player.paused) {
+    player[paused ? "pause" : "play"]();
+  }
+  if (muted !== undefined) {
+    if (muted !== player.muted) {
+      player.muted = muted;
+    }
+    if (volume !== player.volume) {
+      player.volume = volume;
+    }
+  }
+  if (speed && speed !== player.playbackRate) {
+    player.playbackRate = speed;
   }
 };
 
