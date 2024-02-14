085c0ca15b5e760b2fafa6c36b9be00332cdad27
aidenlx <31102694+aidenlx@users.noreply.github.com>
2024-02-15 01:43:15 +0800

    fix(app): fix unstable window migration

--- apps/app/src/lib/window-migration.ts
+++ apps/app/src/lib/window-migration.ts
@@ -2,44 +2,90 @@ import type { PlayerComponent } from "@/media-view/base";
 import { onPlayerMounted } from "../components/context";
 
 export interface LastState {
-  win: Window | undefined;
   currentTime: number;
   playbackRate: number;
   paused: boolean;
 }
 
 export function handleWindowMigration<
-  T extends PlayerComponent & { containerEl: HTMLElement },
->(player: T, onWindowMigrated: (player: T) => void) {
-  let lastState: LastState | null = null;
-  player.register(
-    onPlayerMounted(player.store, (player) => [
-      player.subscribe(({ currentTime, paused, playbackRate }) => {
-        if (currentTime === 0) return;
-        lastState = {
-          win: player.el?.win,
-          currentTime,
-          paused,
-          playbackRate,
-        };
-      }),
-      player.listen("can-play", (evt) => {
-        if (!lastState || lastState.win === evt.target.el?.win) return;
-        const { currentTime, paused, playbackRate } = lastState;
-        const player = evt.target;
-        player.currentTime = currentTime;
-        player.playbackRate = playbackRate;
-        if (!paused) player.play(new Event("recover-state"));
-        lastState = null;
-      }),
-      player.listen("source-change", (evt) => {
-        // if during window migration, don't reset state
-        if (lastState?.win !== evt.target.el?.win) return;
-        lastState = null;
-      }),
-    ]),
+  T extends PlayerComponent & {
+    containerEl: HTMLElement;
+  },
+>(playerComp: T, onMigrated: () => void) {
+  const lastStateStore = new WeakMap<Window, LastState>();
+  let prevWin: Window | undefined;
+  playerComp.register(
+    onPlayerMounted(playerComp.store, (player) => {
+      return [
+        player.subscribe(({ currentTime, paused, playbackRate }) => {
+          if (currentTime === 0) return;
+          if (!player.el) return;
+          lastStateStore.set(player.el.win, {
+            currentTime,
+            paused,
+            playbackRate,
+          });
+        }),
+        player.listen("can-play", () => {
+          if (!prevWin) return;
+          const lastState = lastStateStore.get(prevWin);
+          if (!lastState) {
+            prevWin = undefined;
+            return;
+          }
+          const { currentTime, paused, playbackRate } = lastState;
+          if (!paused) {
+            player
+              .play(new Event("recover-state"))
+              .then(() => {
+                player.currentTime = currentTime;
+                player.playbackRate = playbackRate;
+                if (prevWin) {
+                  lastStateStore.delete(prevWin);
+                  prevWin = undefined;
+                }
+              })
+              .catch((e) => console.error("recov err play", e));
+          } else {
+            try {
+              player.currentTime = currentTime;
+              player.playbackRate = playbackRate;
+              if (prevWin) {
+                lastStateStore.delete(prevWin);
+                prevWin = undefined;
+              }
+            } catch (e) {
+              console.error("recov err paused", e);
+            }
+          }
+        }),
+      ];
+    }),
   );
-  player.register(
-    player.containerEl.onWindowMigrated(() => onWindowMigrated(player)),
+  playerComp.register(
+    onWindowMigrated(playerComp.containerEl, (_, prev) => {
+      onMigrated();
+      if (lastStateStore.has(prev)) {
+        prevWin = prev;
+      } else {
+        prevWin = undefined;
+      }
+    }),
   );
+  playerComp.register(() => {
+    prevWin = undefined;
+  });
+}
+
+function onWindowMigrated(
+  target: HTMLElement,
+  handler: (win: Window, prevWin: Window) => void,
+) {
+  let prevWin = target.win;
+  return target.onNodeInserted(() => {
+    const currWin = target.win;
+    if (currWin === prevWin) return;
+    handler(currWin, prevWin);
+    prevWin = currWin;
+  });
 }
