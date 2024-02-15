609e91ba6e6f96a5e695d0424f4e30ed03713c64
aidenlx <31102694+aidenlx@users.noreply.github.com>
2024-02-15 10:22:48 +0800

    chore: v3.0.0-alpha.9

--- apps/app/scripts/zip.mjs
+++ apps/app/scripts/zip.mjs
@@ -1,4 +1,4 @@
-import { createReadStream, createWriteStream } from "fs";
+import { createReadStream, createWriteStream, readFileSync } from "fs";
 import JSZip from "jszip";
 import { join } from "path";
 import { pipeline } from "stream/promises";
@@ -7,8 +7,10 @@ const zip = new JSZip();
 for (const filename of assets) {
   zip.file(filename, createReadStream(join("dist", filename)));
 }
+const version = JSON.parse(readFileSync(join("manifest.json"), "utf-8")).version;
+const out = join("dist", `mx-${version}.zip`)
 await pipeline(
   zip.generateNodeStream({ type: "nodebuffer", streamFiles: true }),
-  createWriteStream(join("dist", "media-extended.zip")),
+  createWriteStream(out),
 );
-console.log("build.zip written.");
+console.log(out + " written.");
