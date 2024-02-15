d7e5581db3ed99db879626de5b9a5b7169b41777
aidenlx <31102694+aidenlx@users.noreply.github.com>
2024-02-15 11:22:08 +0800

    build: add release-it

---
+++ apps/app/.release-it.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+  hooks: {
+    "before:init": ["pnpm run type-check"],
+    "after:bump": [
+      "pnpm build",
+      "node ./scripts/zip.mjs",
+      "git add ../../.",
+    ],
+    "after:release":
+      "echo Successfully released obsidian plugin ${name} v${version} to ${repo.repository}.",
+  },
+  git: {
+    commitMessage: "chore: release obsidian plugin v${version}",
+    tagName: "${version}",
+    tagAnnotation: "Release Obsidian Plugin v${version}",
+    addUntrackedFiles: true,
+  },
+  plugins: {
+    // "@release-it/conventional-changelog": {
+    //   preset: "angular",
+    //   infile: "CHANGELOG.md",
+    // },
+    "./scripts/ob-bumper.mjs": {
+      indent: 2,
+      copyTo: "../..",
+    },
+  },
+  npm: {
+    publish: false,
+  },
+  github: {
+    release: true,
+    assets: [
+      "dist/main.js",
+      "dist/manifest.json",
+      "dist/styles.css",
+      "dist/mx-*.zip",
+    ],
+    proxy: process.env.HTTPS_PROXY,
+    releaseName: "${version}",
+  },
+};
