86df71c932415f01c286b678fcb5c5c187f1681b
aidenlx <31102694+aidenlx@users.noreply.github.com>
2024-02-21 18:17:55 +0800

    docs: update ICP

--- apps/docs/components/ICP.tsx
+++ apps/docs/components/ICP.tsx
@@ -1,16 +1,21 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function ICP() {
   const { locale } = useRouter();
   if (locale !== "zh-CN") return null;
+  const [isPkmerCN, setIsPkmerCN] = useState(false);
+  useEffect(() => {
+    window.location.hostname.includes("pkmer.cn") && setIsPkmerCN(true);
+  },[])
   return (
     <a
-      className="text-xs"
+      className={"text-xs " + (isPkmerCN ? "" : "hidden")}
       href="https://beian.miit.gov.cn/"
       target="_blank"
       rel="noreferrer"
     >
-      闽ICP备19020233号-1
+      京ICP备2023005152号
     </a>
   );
 }
