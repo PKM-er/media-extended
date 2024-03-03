cf05522b9537a5f8e1e603f81647755fa8c9ed65
aidenlx <31102694+aidenlx@users.noreply.github.com>
2024-03-04 07:41:05 +0800

    chore(docs): add ownership

--- apps/docs/pages/_app.jsx
+++ apps/docs/pages/_app.jsx
@@ -1,10 +1,17 @@
 import "../styles.css";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="ly6FGyn_FKEZHihe55ClBlOQtQIB4UyFzrNFshsEC70"
+        />
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </>
